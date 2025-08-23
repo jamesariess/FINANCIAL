4e493228a03e98cec3db4ae5697eed5879f0ce22
jamesariess <jamesariesconcepcion@gmail.com>
2025-08-23 14:39:10 +0800

    Create receipt.php

---
+++ crud/collection/receipt.php
@@ -0,0 +1,66 @@
+<?php
+include_once __DIR__ . '/../../utility/connection.php';
+
+date_default_timezone_set('Asia/Manila');
+
+$id = $_GET['id'] ?? null;
+$sql = "SELECT * FROM receipt WHERE receiptID = :id";
+$stmt = $pdo['collection']->prepare($sql);
+$stmt->bindParam(':id', $id);
+$stmt->execute();
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+   
+    if (isset($_POST['archive'])) {
+        $receiptID = $_POST['receiptID'];
+        $sql = "UPDATE receipt SET Archive = 'YES' WHERE receiptID = :receiptID";
+        $stmt = $pdo['collection']->prepare($sql);
+        $stmt->bindParam(':receiptID', $receiptID);
+
+        try {
+            $stmt->execute();
+            echo "✅ Payment archived successfully.";
+        } catch (PDOException $e) {
+            echo "❌ Error: " . $e->getMessage();
+        }
+    }
+    if(isset($_POST['update'])){
+        $receiptID = $_POST['receiptID'];
+        $issueBy = $_POST['issueBy'];
+   
+
+        if (empty($issueBy)) {
+            echo "Don't Leave Empty.";
+            exit;
+        } else {
+            $sql = "UPDATE receipt SET issueBy = :issueBy WHERE receiptID = :receiptID";
+            $stmt = $pdo['collection']->prepare($sql);
+            $stmt->bindParam(':issueBy', $issueBy);
+            $stmt->bindParam(':receiptID', $receiptID);
+     
+
+            try {
+                $stmt->execute();
+                echo "✅ issueBy updated successfully.";
+            } catch (PDOException $e) {
+                echo "❌ Error: " . $e->getMessage();
+            }
+        }
+    }
+    
+   
+}
+
+
+
+try {
+    $sql = "SELECT * FROM receipt WHERE Archive = 'NO'
+            ORDER BY receiptsdate Asc";
+    $stmt = $pdo['collection']->query($sql);
+    $receipts = $stmt->fetchAll(PDO::FETCH_ASSOC);
+} catch (PDOException $e) {
+    echo "❌ Error fetching plans: " . $e->getMessage();
+}
+
+
+?>
