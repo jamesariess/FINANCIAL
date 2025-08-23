320c322b4e3bc4d592d617fd3a5ee6cbabbed84b
jamesariess <jamesariesconcepcion@gmail.com>
2025-08-23 14:38:37 +0800

    Update connection.php

--- utility/connection.php
+++ utility/connection.php
@@ -2,13 +2,23 @@
 $server   = "localhost";
 $username = "root";
 $password = "";
-$db       = "financial_collection";
+$db       = [
+    "collection"=> "financial_collection",
+    "disbursement"=>"financial_disbursement",
+    "general"=> "financial_general_ledger",
+    "budget"=> "financial_budget_management",
+    "account"=>"financial_account_payable"
+];
+
+$pdo=[];
 
 try {
-    $pdo = new PDO("mysql:host=$server;dbname=$db", $username, $password);
-    // Correct spelling: setAttribute, and correct constant name
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    foreach ($db as $key => $dbname) {
+        $pdo[$key] = new PDO("mysql:host=$server;dbname=$dbname", $username, $password);
+        $pdo[$key]->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
 } catch (PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
 }
+
 ?>
