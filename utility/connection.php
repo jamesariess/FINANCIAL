9a61cfd80f70c85f7356bf83d28fd38bc3e79a44
jamesariess <jamesariesconcepcion@gmail.com>
2025-08-31 11:27:13 +0800

    Update connection.php

--- utility/connection.php
+++ utility/connection.php
@@ -3,12 +3,12 @@ $server   = "localhost";
 $username = "root";
 $password = "";
 $db       = [
-    "collection"=> "financial_collection",
-    "disbursement"=>"financial_disbursement",
-    "general"=> "financial_general_ledger",
-    "budget"=> "financial_budget_management",
-    "account"=>"financial_account_payable",
-    "ar"=>"financial_account_receivable"
+    "collection"=> "financial",
+    "disbursement"=>"financial",
+    "general"=> "financial",
+    "budget"=> "financial",
+    "account"=>"financial",
+    "ar"=>"financial"
 ];
 
 $pdo=[];
