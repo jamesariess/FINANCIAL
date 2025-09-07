21c01a386406c565080325370d6535eb8dd0a158
jamesariess <jamesariesconcepcion@gmail.com>
2025-09-07 14:12:36 +0800

    Update connection.php

--- utility/connection.php
+++ utility/connection.php
@@ -1,15 +1,16 @@
 
 <?php
 $host = "localhost"; // or yourdomain.com
-$port = 3306;
+
 $dbname = "financial";
 $username = "root";
 $password = "";
 
 try {
-    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname", $username, $password);
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 } catch (PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
 }
+?>
