59533fcae32a4ba3e2284fbe5962a8aa242a4a6d
jamesariess <jamesariesconcepcion@gmail.com>
2025-09-07 11:33:55 +0800

    Create dashboard.php

---
+++ pages/dashboard/dashboard.php
@@ -0,0 +1,30 @@
+<?php include __DIR__ . "/../sidebar.html"; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Adjustments</title>
+</head>
+<body> 
+<div class="content" id="mainContent">
+    <div class="header">
+        <div class="hamburger" id="hamburger">☰</div>
+        <div>
+            <h1>Disbursement Dashboard <span class="system-title">| (NAME OF DEPARTMENT)</span></h1>
+        </div>
+        <div class="theme-toggle-container">
+            <span class="theme-label">Dark Mode</span>
+            <label class="theme-switch">
+                <input type="checkbox" id="themeToggle">
+                <span class="slider"></span>
+            </label>
+        </div>
+    </div>
+hello
+
+
+</div>
+<script src="<?php echo '/financial/static/js/modal.js'; ?>"></script>
+</body>
+</html>
