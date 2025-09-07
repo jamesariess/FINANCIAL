7450130d520f78ab61b580439f8fe28cfcad6256
jamesariess <jamesariesconcepcion@gmail.com>
2025-09-07 15:49:53 +0800

    Update paymentmethod.php

--- crud/disbursement/paymentmethod.php
+++ crud/disbursement/paymentmethod.php
@@ -5,7 +5,7 @@ date_default_timezone_set('Asia/Manila');
 
 $id = $_GET['id'] ?? null;
 $sql = "SELECT * FROM paymentmethod WHERE payment_method_id = :id";
-$stmt = $pdo['budget']->prepare($sql);
+$stmt = $pdo->prepare($sql);
 $stmt->bindParam(':id', $id);
 $stmt->execute();
 
@@ -16,7 +16,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $archive = 'YES';
 
         $sql = "UPDATE paymentmethod SET Archive = :archive WHERE payment_method_id = :payment_method_id";
-        $stmt = $pdo['budget']->prepare($sql);
+        $stmt = $pdo->prepare($sql);
         $stmt->bindParam(':archive', $archive);
         $stmt->bindParam(':payment_method_id', $payment_method_id);
 
@@ -34,7 +34,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $status = $_POST['update_status'];
 
         $sql = "UPDATE paymentmethod SET method_name = :method_name, account_details = :account_details, status = :status WHERE payment_method_id = :payment_method_id";
-        $stmt = $pdo['budget']->prepare($sql);
+        $stmt = $pdo->prepare($sql);
         $stmt->bindParam(':method_name', $method_name);
         $stmt->bindParam(':account_details', $account_details);
         $stmt->bindParam(':status', $status);
@@ -53,7 +53,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 try {
     $sql = "SELECT * FROM  paymentmethod WHERE Archive = 'NO'
              ORDER BY method_name Asc";
-    $stmt = $pdo['budget']->query($sql);
+    $stmt = $pdo->query($sql);
     $paymentmethodReports = $stmt->fetchAll(PDO::FETCH_ASSOC);
 } catch (PDOException $e) {
     echo "❌ Error fetching plans: " . $e->getMessage();
