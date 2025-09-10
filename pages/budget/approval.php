350c112f683afe2e9f1b0556d82625212bff4446
jamesariess <jamesariesconcepcion@gmail.com>
2025-09-10 17:44:22 +0800

    Create approval.php

---
+++ pages/budget/approval.php
@@ -0,0 +1,42 @@
+
+<?php include __DIR__ . "/../sidebar.html"; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Budget Adjustment </title>
+    <link rel="stylesheet" href="/static/css/sidebar.css">
+    
+</head>
+
+<body> 
+<div class="content" id="mainContent">
+    <div class="header">
+        <div class="hamburger" id="hamburger">☰</div>
+        <div>
+            <h1> Approval <span class="system-title">| (NAME OF DEPARTMENT)</span></h1>
+        </div>
+        <div class="theme-toggle-container">
+            <span class="theme-label">Dark Mode</span>
+            <label class="theme-switch">
+                <input type="checkbox" id="themeToggle">
+                <span class="slider"></span>
+            </label>
+        </div>
+    </div>
+<?php include __DIR__ . '/../../filtering/budget/adjustment.html'; ?>
+<br>
+    
+
+    <?php include __DIR__ . '/../../table/budgetmanagement.html/adjustmenttable.html';?>
+
+</div>
+<script src="<?php echo '/financial/static/js/filter.js';?>"></script>
+<script src="<?php echo '/financial/static/js/modal.js'; ?>"></script>
+
+
+
+</body>
+
+</html>
