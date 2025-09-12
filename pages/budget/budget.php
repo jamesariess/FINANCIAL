ce291288f54b6b021ff5b3b3aa9cd16524030c13
jamesariess <jamesariesconcepcion@gmail.com>
2025-09-12 20:39:28 +0800

    Create budget.php

---
+++ pages/budget/budget.php
@@ -0,0 +1,40 @@
+<?php include __DIR__ . "/../sidebar.html"; ?> 
+      <?php include __DIR__ . '/../../crud/budget/budget.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Budget Allocation </title>
+    <link rel="stylesheet" href="/static/css/sidebar.css">
+    
+</head>
+
+<body> 
+<div class="content" id="mainContent">
+    <div class="header">
+        <div class="hamburger" id="hamburger">☰</div>
+        <div>
+            <h1>Disbursement Dashboard <span class="system-title">| (NAME OF DEPARTMENT)</span></h1>
+        </div>
+        <div class="theme-toggle-container">
+            <span class="theme-label">Dark Mode</span>
+            <label class="theme-switch">
+                <input type="checkbox" id="themeToggle">
+                <span class="slider"></span>
+            </label>
+        </div>
+    </div>
+
+    <?php include __DIR__ . '/../../cards/budget/budget.php'; ?>
+    <?php include __DIR__ . '/../../ai/budget/budget.php'; ?>
+  <?php include __DIR__ . '/../../contents/budget/budget.php'; ?>
+<br>
+
+</div>
+<script src="<?php echo '/financial/static/js/filter.js';?>"></script>
+<script src="<?php echo '/financial/static/js/modal.js'; ?>"></script>
+
+</body>
+
+</html>
