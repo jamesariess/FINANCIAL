a3ab9dcbdd0fb532aa6f8172f27fdd8aaf4d52cb
jamesariess <jamesariesconcepcion@gmail.com>
2025-09-16 17:04:08 +0800

    Update invoce.php

--- pages/collection/invoce.php
+++ pages/collection/invoce.php
@@ -29,8 +29,8 @@
     <?php include __DIR__ . '/../../table/collection/invoice.html';?>
 
 </div>
-<script src="<?php echo '/financial/static/js/modal.js'; ?>"></script>
-
+<script src="<?php echo '/../static/js/filter.js';?>"></script>
+<script src="<?php echo '/../static/js/modal.js'; ?>"></script>
 
 </body>
 
