4f0ab78c66d906c20acd4c5cdf36eac176832251
jamesariess <jamesariesconcepcion@gmail.com>
2025-09-16 11:50:26 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,16 @@
 <?php
-// index.php
-// This is the main entry point for the SLATE application.
-// It redirects users to either the login page or the dashboard based on their session status.
 
-session_start(); // Start the PHP session to manage user login state
 
-// Check if the user is logged in
+session_start();
+
+
 if (isset($_SESSION['logged_in']) && $_SESSION['logged_in'] === true) {
    
-    header("Location: pages/ar/adjustment.php");
-    exit(); // Always exit after a header redirect
+    header("Location: pages/dashboard/dashboard.php");
+    exit(); 
 } else {
  
     header("Location: partials/login.php");
-    exit(); // Always exit after a header redirect
+    exit();
 }
 ?>
