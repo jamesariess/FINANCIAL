ae329f22eda75ef3a4b93cef6ca2937dd27ec9d0
jamesariess <jamesariesconcepcion@gmail.com>
2025-09-16 17:04:06 +0800

    Update approver.php

--- pages/disbursement/approver.php
+++ pages/disbursement/approver.php
@@ -28,7 +28,7 @@
    
     <?php include __DIR__ . '/../../contents/disbursement/approver.php';?>
 </div>
-<script src="<?php echo '/financial/static/js/filter.js';?>"></script>
-<script src="<?php echo '/financial/static/js/modal.js'; ?>"></script>
+<script src="<?php echo '/../static/js/filter.js';?>"></script>
+<script src="<?php echo '/../static/js/modal.js'; ?>"></script>
 </body>
 </html>
