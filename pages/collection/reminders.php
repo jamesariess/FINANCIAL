7bda2a3090f3b03aee7d43711de950c89052bf0d
jamesariess <jamesariesconcepcion@gmail.com>
2025-09-16 17:03:59 +0800

    Update reminders.php

--- pages/collection/reminders.php
+++ pages/collection/reminders.php
@@ -37,9 +37,8 @@
        </div>
 </div>
  </div>
-<script src="<?php echo '/financial/static/js/filter.js';?>"></script>
-<script src="<?php echo '/financial/static/js/modal.js'; ?>"></script>
-
+<script src="<?php echo '/../static/js/filter.js';?>"></script>
+<script src="<?php echo '/../static/js/modal.js'; ?>"></script>
 
 </body>
 
