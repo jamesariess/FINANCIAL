341949546a52bf502ace7abb97d6ed38d28a47d7
jamesariess <jamesariesconcepcion@gmail.com>
2025-09-16 17:04:45 +0800

    Update loan.php

--- pages/ap/loan.php
+++ pages/ap/loan.php
@@ -33,8 +33,8 @@
  
 
 </div>
-<script src="<?php echo '/financial/static/js/filter.js';?>"></script>
-<script src="<?php echo '/financial/static/js/modal.js'; ?>"></script>
+<script src="<?php echo '/../static/js/filter.js';?>"></script>
+<script src="<?php echo '/../static/js/modal.js'; ?>"></script>
 
 
 
