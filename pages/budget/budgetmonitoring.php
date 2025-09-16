68b69c2bb083bac4c3c883287faf3c3a0285439f
jamesariess <jamesariesconcepcion@gmail.com>
2025-09-16 17:04:13 +0800

    Update budgetmonitoring.php

--- pages/budget/budgetmonitoring.php
+++ pages/budget/budgetmonitoring.php
@@ -29,7 +29,8 @@
 
 
 </div>
-<script src="<?php echo '../../static/js/modal.js'; ?>"></script>
+<script src="<?php echo '/../static/js/filter.js';?>"></script>
+<script src="<?php echo '/../static/js/modal.js'; ?>"></script>
 
 
   
