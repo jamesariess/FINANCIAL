1d99f524b23a930da6bb9ed68ab3570343223cd8
jamesariess <jamesariesconcepcion@gmail.com>
2025-09-16 16:27:42 +0800

    Update connection.php

--- utility/connection.php
+++ utility/connection.php
@@ -4,9 +4,9 @@
 
 // Database connection
 $host = "localhost";
-$dbname = "financial";
-$username = "root";
-$password = "";
+$dbname = "fina_financial";
+$username = "fina_finances";
+$password = "7rO-@mwup07Io^g0";
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
