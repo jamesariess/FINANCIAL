9b1d4b5d71fc4617a17d35ea6099e4e14bea2973
jamesariess <jamesariesconcepcion@gmail.com>
2025-09-16 17:03:23 +0800

    Create ap_adjustment copy.php

---
+++ pages/ap/ap_adjustment copy.php
@@ -0,0 +1,41 @@
+<?php include __DIR__ . '/../../crud/ap/adjustment.php';?>
+<?php include __DIR__ . "/../sidebar.html"; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AR Adjustment </title>
+    <link rel="stylesheet" href="/static/css/sidebar.css">
+    
+</head>
+
+<body> 
+<div class="content" id="mainContent">
+    <div class="header">
+        <div class="hamburger" id="hamburger">☰</div>
+        <div>
+            <h1>Invoice Adjusment<span class="system-title">| (Financial)</span></h1>
+        </div>
+        <div class="theme-toggle-container">
+            <span class="theme-label">Dark Mode</span>
+            <label class="theme-switch">
+                <input type="checkbox" id="themeToggle">
+                <span class="slider"></span>
+            </label>
+        </div>
+    </div>
+<?php include __DIR__ . '/../../filtering/ap/adjustment.html'; ?>
+<br>
+    <?php include __DIR__ . '/../../modal/ap/adjustment.html'; ?>
+    <?php include __DIR__ . '/../../table/ap/adjustment.php';?>
+
+</div>
+<script src="<?php echo '/../static/js/filter.js';?>"></script>
+<script src="<?php echo '/../static/js/modal.js'; ?>"></script>
+
+
+
+</body>
+
+</html>
