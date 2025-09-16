0eab237aa1fbcd90c1e57ac3e4260d5dc5c05958
jamesariess <jamesariesconcepcion@gmail.com>
2025-09-16 17:04:28 +0800

    Update budgetadjustment.php

--- pages/budget/budgetadjustment.php
+++ pages/budget/budgetadjustment.php
@@ -34,8 +34,8 @@
 
 </div>
 <
-<script src="<?php echo '../../static/js/filter.js';?>"></script>
-<script src="<?php echo '../../static/js/modal.js'; ?>"></script>
+<script src="<?php echo '/../static/js/filter.js';?>"></script>
+<script src="<?php echo '/../static/js/modal.js'; ?>"></script>
 
 
 
