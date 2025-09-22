9239ba3fd4b5f9419e3e86f40a50595c7d8b5cbd
jamesariess <jamesariesconcepcion@gmail.com>
2025-09-22 14:45:52 +0800

    Update header.php

--- static/head/header.php
+++ static/head/header.php
@@ -1,5 +1,5 @@
 <link rel="stylesheet" href="<?php echo '../../static/css/filter.css';?>">
-    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
