873b99e7f4a21fddeaadf620f72bdfd98ef6baeb
jamesariess <jamesariesconcepcion@gmail.com>
2025-09-23 12:27:53 +0800

    Update dashboard.php

--- pages/dashboard/dashboard.php
+++ pages/dashboard/dashboard.php
@@ -170,7 +170,7 @@ try {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Approver Management</title>
-    <link rel="stylesheet" href="../../../static/css/sidebar.css">
+    <link rel="stylesheet" href="../../../../static/css/sidebar.css">
 <style>
     
 </style>
